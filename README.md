b24fd20ab5706df55c4fb5b5d32917448f705228
Rommel7 <ramil.mamedov7991@gmail.com>
2018-04-20 01:00:15 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## AMET | Manpower Equipment Transportation
+
+## Install 😊
+```sh
+$ git clone https://github.com/Rommel7/AMET.git
+$ npm i 
+```
+***or***
+```
+$ yarn add
+```
---
+++ app/js/core.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('.owl-two').owlCarousel({
+    items: 4,
+    loop: true,
+    nav: true,
+    dots: true
+  });
+  $('.owl-three').owlCarousel({
+    items: 6,
+    loop: true,
+    nav: true,
+    dots: true
+  });
+});

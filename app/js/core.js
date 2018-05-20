0b60b92c6942a18de118c566032bcf02d9591ae8
Ramil Mamedov <ramil.mamedov7991@gmail.com>
2018-05-20 12:59:14 +0400

    js updated modal removed

--- app/js/core.js
+++ app/js/core.js
@@ -1,20 +1,20 @@
-var modal = document.getElementById("myModal");
-var span = document.getElementById("close");
+// var modal = document.getElementById("myModal");
+// var span = document.getElementById("close");
 
-var images = document.getElementsByClassName("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-var i;
-for (i = 0; i < images.length; i++) {
-  images[i].onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    modalImg.alt = this.alt;
-  };
-}
-span.onclick = function () {
-  modal.style.display = "none";
-};
+// var images = document.getElementsByClassName("myImg");
+// var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+// var i;
+// for (i = 0; i < images.length; i++) {
+//   images[i].onclick = function () {
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     modalImg.alt = this.alt;
+//   };
+// }
+// span.onclick = function () {
+//   modal.style.display = "none";
+// };
 
 $(document).ready(function () {
   $(window).on('load', function () {
@@ -41,9 +41,9 @@ $(document).ready(function () {
     dots: true
   });
 
-  $(document).keyup(function (e) {
-    if (e.keyCode == 27) {
-      modal.style.display = "none";
-    }
-  });
+  // $(document).keyup(function (e) {
+  //   if (e.keyCode == 27) {
+  //     modal.style.display = "none";
+  //   }
+  // });
 });
